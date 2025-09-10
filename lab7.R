b6bcad055a28a2291367bb6011ce4c0fd7b5999a
kayvour <vaidyakaivalya77@gmail.com>
2025-09-10 08:31:10 +0530

    Create lab7.R

---
+++ lab7.R
@@ -0,0 +1,15 @@
+# normal distribution - continuous random variable
+# pdf of mean m and sd sigma is given by:
+# f(x) = 1/(sigma x root(2 pi)) x e^(x - mu/2 sigma)^2, x and u b/w - infinity and infinity
+# standard curve - bell
+
+# standard normal distribution
+# mean is 0, sd is 1
+# mgf = e^(ut + t^2 sigma^2/2)
+
+# Q.1 A normal distribution has my 20 and sd is sigma. The normal variate z = (x - mu)/sigma. P(15 <= x <= 40) = ?
+# when x = 15, z = -0.5
+# when x = 40, z = 2
+
+# to find area under the given curve (bell)
+# 
