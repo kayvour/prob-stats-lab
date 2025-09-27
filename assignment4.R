017dd27a9e59c414593c73e1171dc4f9ab0f08db
kayvour <vaidyakaivalya77@gmail.com>
2025-09-27 22:18:15 +0530

    new

---
+++ assignment4.R
@@ -0,0 +1,7 @@
+# 1. A factory claims that the average lifetime of its LED bulbs is 2,000 hours. A random sample of 120 bulbs shows a mean lifetime of 1,950 hours with a standard deviation of 150 hours. At the 1% level of significance, test whether the company’s claim is valid.
+
+# 2. A random sample of 400 students in college A has a mean score of 55 in an aptitude test with a standard deviation of 10. Another independent sample of 500 students in college B has a mean score of 57 with a standard deviation of 12. Can we conclude at the 5% significance level that students of college B perform better than those of college A?
+
+# 3. A manufacturer claims that 95% of the items produced are defect-free. Out of 600 items chosen at random, 570 items are found to be defect-free. Test at the 5% level of significance whether the claim is true.
+
+# 4. A survey of 1,000 families in rural areas showed that 400 families own a television. A similar survey of 1,200 families in urban areas showed that 700 families own a television. Test whether there is a significant difference between the proportions of families owning a TV in rural and urban areas at the 1% level of significance.
--- lab8.R
+++ lab8.R
@@ -49,3 +49,61 @@ if(z < z_critical){
 } else{
   cat("Fail to reject H0: No evidence of efficiency")
 }
+
+# In a random sample of size 500, the mean is found to be 20. In another independent sample of size 400, the mean is 15.
+# Could the samples have been drawn from the same population with sd 4?
+n1 = 500
+n2 = 400
+xbar1 = 20
+xbar2 = 15
+sigma = 4
+alpha = 0.05
+
+# type 2 - diff of 2 mean test
+z = (xbar1 - xbar2)/sqrt((sigma * sigma/n1) + (sigma * sigma/n2))
+z
+
+z_critical = qnorm(1 - alpha/2)
+z_critical
+
+if(abs(z) > z_critical){
+  cat("Reject H0: No significant difference")
+} else{
+  cat("Fail to reject H0: There is a significant difference between the samples")
+}
+
+# In a large city A, 20% of a random sample of 950 school boys had a slight physical defect. In another large city B, 18.5% of a random sample of 1600 school boys had the same defect.
+# Is the difference between the proportions significant?
+p1 = 0.20
+n1 = 900
+p2 = 0.185
+n2 = 1600
+alpha = 0.05
+# P = combined proportions
+P = (p1*n1 + p2*n2)/(n1 + n2)
+P
+Q = 1 - P
+
+# type 4 - diff of 2 proportions
+z = (p1 - p2)/sqrt(P*Q*((1/n1) + (1/n2)))
+z
+
+# counts
+x1 = round(p1 * n1)
+x2 = round(p2 * n2)
+
+# pooled proportion - population
+p_pool = (x1 + x2)/(n1 + n2)
+
+# test statistic - 2 tailed
+z = (p1 - p2)/sqrt(p_pool * (1 - p_pool) * (1/n1 + 1/n2))
+z
+
+z_critical = qnorm(1 - alpha/2)
+z_critical
+
+if(abs(z) > z_critical){
+  cat("H0 rejected: Proportions difference is significant")
+} else{
+  cat("Fail to reject: Proportions difference is not significant")
+}
