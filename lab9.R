b0201723e25b75dadaa4a18b3fa8ce17d581fc15
kayvour <vaidyakaivalya77@gmail.com>
2025-10-29 09:09:40 +0530

    Update lab9.R

--- lab9.R
+++ lab9.R
@@ -64,3 +64,42 @@ if(p_value_2 < 0.05){
 } else {
   cat("Fail to reject the null hypothesis: there is no significant difference in the average sales between salesmen\n")
 }
+
+# 3. Consider analyzing the productivity of 5 kinds of manure, five kinds of cultivation, and five kinds of crops. As follows the data is organized in a latin square format
+manure = c(rep("manure1", 1), rep("manure2", 1), rep("manure3", 1), rep("manure4", 1), rep("manure5", 1))
+cultivation = c(rep("cultP", 5), rep("cultQ", 5), rep("cultR", 5), rep("cultS", 5), rep("cultT", 5))
+crop = c("P", "T", "R", "Q", "S", "R", "Q", "P", "S", "T", "Q", "R", "S", "T", "P", "S", "P", "T", "R", "Q", "T", "S", "Q", "P", "R")
+freq = c(42, 45, 41, 56, 47, 47, 54, 46, 52, 49, 55, 52, 57, 49, 45, 51, 44, 47, 50, 54, 44, 50, 48, 43, 46)
+
+#The three factors are: manure (man1:5), cultivation(cultP:T), crop(P:T)
+
+data = data.frame(manure, cultivation, crop, freq)
+data
+
+res = aov(freq ~ as.factor(manure) + as.factor(cultivation) + as.factor(crop), data = data)
+summary(res)
+
+p_manure = summary(res)[[1]][["Pr(>F)"]][1]
+p_cultivation = summary(res)[[1]][["Pr(>F)"]][2]
+p_crop = summary(res)[[1]][["Pr(>F)"]][3]
+p_manure
+p_cultivation
+p_crop
+
+if(p_manure < 0.05){
+  cat("Reject H0 for manure: there is a significant difference among manures.\n")
+} else {
+  cat("Fail to reject H0 for manure: no significant difference among manures.\n")
+}
+
+if(p_cultivation < 0.05){
+cat("Reject H0 for cultivation: there is a significant difference among cultivation methods.\n")
+} else {
+cat("Fail to reject H0 for cultivation: no significant difference among cultivation methods.\n")
+}
+
+if(p_crop < 0.05){
+cat("Reject H0 for crop: there is a significant difference among crops.\n")
+} else {
+cat("Fail to reject H0 for crop: no significant difference among crops.\n")
+}
